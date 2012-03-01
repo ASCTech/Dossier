e1cb4d71312e3d656a4fbe2cdd8ece01ed8501dc
John Colvin <colvin.john@gmail.com>
2012-03-01 12:34:14 -0500

    Refactored rendering 403 and 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,20 +9,19 @@ protected
 
   def render_403(message='')
     message = 'Not authorized' if message.empty?
-    respond_to do |format|
-      format.html { render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false }
-      format.json { render :json => { :error => true, :message => message } }
-      format.xml  { render :xml  => { :error => true, :message => message } }
-    end
+    render_error 403, message
   end
 
   def render_404(message='')
     message = 'Resource not found' if message.empty?
+    render_error 404, message
+  end
+
+  def render_error(status, message)
     respond_to do |format|
-      format.html { render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false }
+      format.html { render :file => "#{Rails.root}/public/#{status}.html", :status => status, :layout => false }
       format.json { render :json => { :error => true, :message => message } }
       format.xml  { render :xml  => { :error => true, :message => message } }
     end
   end
-
 end
