f3623553369813132d1fc5fd5de4685a869e0c57
John Colvin <colvin.john@gmail.com>
2012-03-01 11:19:10 -0500

    Changed upload path

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -13,7 +13,7 @@ class FileUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    Rails.root + "uploads/#{model.owner_id}"
+    Rails.root + "uploads/#{model.owner_id}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
