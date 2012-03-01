c27a28b5a96519230ba4bd02b8a038229c786b7c
John Colvin <colvin.john@gmail.com>
2012-03-01 12:20:29 -0500

    Refactored rendering 403

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,13 @@ protected
     SourceSystem.find_by_api_key(request.headers['x-api-key'])
   end
 
+  def render_403(message='')
+    message = 'Not authorized' if message.empty?
+    respond_to do |format|
+      format.html { render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false }
+      format.json { render :json => { :error => true, :message => message } }
+      format.xml  { render :xml  => { :error => true, :message => message } }
+    end
+  end
+
 end
--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -16,11 +16,7 @@ private
 
   def require_api_key
     return if requesting_system
-    respond_to do |format|
-      format.html { render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false }
-      format.json { render :json => { :error => true, :message => "Error 403, you are not authorized to access Dossier." } }
-      format.xml  { render :xml  => { :error => true, :message => "Error 403, you are not authorized to access Dossier." } }
-    end
+    render_403 'You do not have a valid API key'
   end
 
 end
