13fd94bd50a935e168088d1ba1c4396676b85960
John Colvin <colvin.john@gmail.com>
2012-03-02 17:15:27 -0500

    Restricted tag names from being numeric

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,6 @@ class Tag < ActiveRecord::Base
   has_many :document_tags
   has_many :documents, :through => :document_tags
 
+  validates_format_of :name, :with => /[\D]/
+
 end
--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Tag do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+
+    it 'should not allow numeric tag names' do
+      Tag.new(:name => 12345).should_not be_valid
+      Tag.new(:name => "123").should_not be_valid
+      Tag.new(:name => '123').should_not be_valid
+      Tag.new(:name => "1-2").should     be_valid
+      Tag.new(:name => "12s").should     be_valid
+      Tag.new(:name => 's12').should     be_valid
+    end
+
+  end
+
 end
