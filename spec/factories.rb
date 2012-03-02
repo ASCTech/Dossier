9d0d35ed17264efb4456ab16593f03cf03d901a5
John Colvin <colvin.john@gmail.com>
2012-03-02 15:50:26 -0500

    Added some factories

---
+++ spec/factories.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+
+  factory :document do
+    owner_id 93507
+    uploader_id 7171202
+    source_system
+  end
+
+  factory :source_system do
+    name 'ENIAC'
+    api_key '20tn20tnjo20480tn4n20429042j'
+  end
+
+end
