386bb4a398547251edd1526c47dde155f2f95132
John Colvin <colvin.john@gmail.com>
2012-03-02 20:28:16 -0500

    Added a special route for filtering by owner

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Dossier::Application.routes.draw do
 
   resources :documents, :only => [:index, :show, :create, :destroy] do
     get 'file'
+    match 'owner/:owner_id', :action=> :index, :on => :collection
     resources :tags, :only => [:create, :destroy]
   end
 
