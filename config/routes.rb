f5d93e932db5ab135738693b08054d1fd6b519ec
John Colvin <colvin.68@osu.edu>
2012-11-28 13:19:15 -0500

    Removed document tag adding and removing to existing documents

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Dossier::Application.routes.draw do
 
   resources :documents, :only => [:index, :show, :create, :destroy] do
     get 'file'
-    resources :tags, :only => [:create, :destroy]
   end
 
   match 'owners/:owner_id/documents', :controller => :documents, :action=> :index
