b95088f542b913b2ed9c80b328e2c67caf0d323e
John Colvin <colvin.68@osu.edu>
2013-01-04 11:05:09 -0500

    Pathname doesn't like integers anymore...I guess

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -13,7 +13,7 @@ class FileUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    Rails.root.join('uploads', model.owner_id, model.source_system_id, model.id)
+    Rails.root.join('uploads', model.owner_id.to_s, model.source_system_id.to_s, model.id.to_s)
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
